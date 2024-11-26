f5a98d79dfbd2a3bc9b29371c9c6579acf702439
ddas4548 <ddas4548@gmail.com>
2024-11-26 23:15:47 +0530

    account page refactoring implemented

--- components/enroll-course.jsx
+++ components/enroll-course.jsx
@@ -5,8 +5,8 @@ import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 import { createCheckoutSession } from "@/app/actions/stripe";
 
-export const EnrollCourse = ({ asLink,course }) => {
-    console.log(course);
+export const EnrollCourse = ({ asLink,courseId }) => {
+    
     const formAction = async(data) => {
         const {url} = await createCheckoutSession(data);
         window.location.assign(url)
@@ -15,9 +15,8 @@ export const EnrollCourse = ({ asLink,course }) => {
     return (
         <>
             <form action={formAction}>
-                <input type="hidden" name="courseId" value={course.id} />
-                <input type="hidden" name="coursePrice" value={course.price} />
-                <input type="hidden" name="courseName" value={course.title} />
+                <input type="hidden" name="courseId" value={courseId} />
+               
                 {asLink ? (
                     <Button
                         type="submit"
