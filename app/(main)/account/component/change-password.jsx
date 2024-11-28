11d517e60179d42bde002e06b5ab082e2d043b96
ddas4548 <ddas4548@gmail.com>
2024-11-28 22:01:10 +0530

    change password functionality applied

--- app/(main)/account/component/change-password.jsx
+++ app/(main)/account/component/change-password.jsx
@@ -1,49 +1,84 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { toast } from "sonner";
+import { changePassword } from "@/app/actions/account";
 
-const ChangePassword = () => {
+const ChangePassword = ({ email }) => {
+  const [passwordState, setPasswordState] = useState({
+    oldPassword: "",
+    newPassword: "",
+  });
+
+  function handleChange(event) {
+    const field = event.target.name;
+    const value = event.target.value;
+    setPasswordState({
+      ...passwordState,
+      [field]: value,
+    });
+  }
+
+  async function doPasswordChange(event) {
+    event.preventDefault();
+    try {
+      await changePassword(
+        email,
+        passwordState.oldPassword,
+        passwordState.newPassword
+      );
+      toast.success("Updated Successfully");
+    } catch (error) {
+      console.error(error);
+      toast.error(`Error: ${error.message}`);
+    }
+  }
+
   return (
     <div>
-    <h5 className="text-lg font-semibold mb-4">
-        Change password :
-    </h5>
-    <form>
+      <h5 className="text-lg font-semibold mb-4">Change password :</h5>
+      <form onSubmit={doPasswordChange}>
         <div className="grid grid-cols-1 gap-5">
-            <div>
-                <Label className="mb-2 block">Old password :</Label>
-                <Input
-                    type="password"
-                    placeholder="Old password"
-                    required=""
-                />
-            </div>
-            <div>
-                <Label className="mb-2 block">New password :</Label>
-                <Input
-                    type="password"
-                    placeholder="New password"
-                    required=""
-                />
-            </div>
-            <div>
-                <Label className="mb-2 block">
-                    Re-type New password :
-                </Label>
-                <Input
-                    type="password"
-                    placeholder="Re-type New password"
-                    required=""
-                />
-            </div>
+          <div>
+            <Label className="mb-2 block">Old password :</Label>
+            <Input
+              type="password"
+              placeholder="Old password"
+              id="oldPassword"
+              name="oldPassword"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <Label className="mb-2 block">New password :</Label>
+            <Input
+              type="password"
+              placeholder="New password"
+              id="newPassword"
+              name="newPassword"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <Label className="mb-2 block">Re-type New password :</Label>
+            <Input
+              type="password"
+              placeholder="Re-type New password"
+              required=""
+            />
+          </div>
         </div>
         {/*end grid*/}
-        <Button className="mt-5" type="submit">
-            Save password
+        <Button className="mt-5 cursor-pointer" type="submit">
+          Save password
         </Button>
-    </form>
-</div>
-  )
-}
+      </form>
+    </div>
+  );
+};
 
-export default ChangePassword
+export default ChangePassword;
--- app/actions/account.js
+++ app/actions/account.js
@@ -2,6 +2,8 @@
 
 import { User } from "@/model/user-model";
 import { revalidatePath } from "next/cache";
+import bcrypt from  "bcryptjs";
+import { validatePassword } from "@/queries/users";
 
 export async function updateUserInfo(email,updatedData) {
    try {
@@ -11,4 +13,22 @@ export async function updateUserInfo(email,updatedData) {
    } catch (error) {
     throw new Error(error);
    } 
+}
+ 
+export async function changePassword(email,oldPassword,newPassword) {
+   const isMatch = await validatePassword(email,oldPassword);
+   if (!isMatch) {
+     throw new Error("pls enter a valid current password"); 
+   }
+
+   const filter = { email: email };
+   const hashedPassword = await bcrypt.hash(newPassword, 5);
+   const updatedData = { password: hashedPassword };
+   try {
+      await User.findOneAndUpdate(filter, updatedData);
+      revalidatePath("/account");
+   } catch (error) {
+      throw new Error(error);
+   }
+
 }
