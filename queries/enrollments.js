ad8fa23c9dc76db7d2b78c1eba544b772758ebec
ddas4548 <ddas4548@gmail.com>
2025-01-08 22:49:49 +0530

    solving the ui in enroll-coursecard

--- queries/enrollments.js
+++ queries/enrollments.js
@@ -1,11 +1,25 @@
 import { Enrollment } from "@/model/enrollment-model";
 
 import { replaceMongoIdInArray } from "@/lib/convertData";
-
+import { Course } from "@/model/course-model";
 
 export async function getEnrollmentsForCourse(courseId) {
-    const enrollments = await Enrollment.find({ course: courseId }).lean();
+    const enrollments = await Enrollment.find({ course: courseId })
+    .populate({
+        path: "course",
+        model: Course,
+    })
+    .lean();
     return replaceMongoIdInArray(enrollments);
+ }
+
+ export async function getEnrollmentsForUser(userId) {
+   try {
+    const enrollments = await Enrollment.find({ student: userId }).lean();
+    return replaceMongoIdInArray(enrollments);
+   } catch (error) {
+     throw new Error(error);
+   }
  }
 
 export async function enrollForCourse (courseId, userId,paymentMethod) {
